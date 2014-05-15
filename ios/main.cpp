3f7679b4a0cc99ce7ee69e75043b1bf8122e8fca
causlayer <causlayer@gmail.com>
2014-05-15 20:50:16 +0800

    Initial Commit

---
+++ ios/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ios
+//
+//  Created by slayer on 14-5-15.
+//  Copyright (c) 2014年 slayer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
